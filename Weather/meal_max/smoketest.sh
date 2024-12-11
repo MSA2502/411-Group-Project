b47f0df6486cef8b452216bfe9bdb160ecc26d19
mmyman <56144148+mmyman@users.noreply.github.com>
2024-12-10 22:47:19 -0500

    fixed smoke tests

--- Weather/meal_max/smoketest.sh
+++ Weather/meal_max/smoketest.sh
@@ -187,6 +187,11 @@ create_location "Rome"
 create_location "San Francisco"  
 create_location "Atlanta" 
 
+get_location_by_id "1"
+get_weather_for_location "1"
+delete_location "1"
+
+
 create_account "test" "test"
 login "test" "test"
 update_password "test" "new_pass"
